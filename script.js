7d387d9ea1b2db160a30ea2edc2f345c9f018118
BIMALESH YADAV <BIMALESH YADAV@DESKTOP-HRL9V5J>
2022-03-11 15:18:04 +0530

    Add project files.

---
+++ script.js
@@ -0,0 +1,78 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+  section.forEach(sec =>{
+
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+      });
+    };
+
+  });
+}
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active');
+}
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active');
+  }
+  var swiper = new Swiper(".home-slider", {
+    spaceBetween: 200,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
+  
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 5,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    loop:true,
+    breakpoints: {
+      0: {
+          slidesPerView: 1,
+      },
+      630: {
+        slidesPerView: 2,
+      },
+      
+    },
+  });
+  function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+  }
+  
+  function fadeOut(){
+    setInterval(loader, 1000);
+  }
+  
+  window.onload = fadeOut;
+
+  
